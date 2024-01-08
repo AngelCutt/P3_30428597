28f5b4ad6f989a6971156b4e27589709df37d7b2
AngelCutt <147731652+AngelCutt@users.noreply.github.com>
2024-01-08 13:53:41 +0100

    Update controllers.js
    cambiando claves de recaptcha

--- controllers.js
+++ controllers.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 var handlebars = require('express-handlebars')
 const Recaptcha = require('express-recaptcha').RecaptchaV2;
-const recaptcha = new Recaptcha('6LfOBUYpAAAAAN8w1Meatl-oxfr8w4EBhh2Cvsj1', '6LfOBUYpAAAAAO5ERFJseNoPrvc9X0ckgVGdPOkD');
+const recaptcha = new Recaptcha('6LcP7EkpAAAAADhHkaAuLH8jC4G3Mp04NgwBgacw', '6LcP7EkpAAAAAGBhwpe7GvkNtCQN9q3Wc5Ma7Nvx');
 const multer = require('multer');
 const http = require('http');
 const express = require('express');
